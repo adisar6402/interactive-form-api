7dd5aa815def45d245fcd6a15d49f16069d22011
adisar6402 <adisar6402@gmail.com>
2024-11-11 14:15:20 +0100

    Updated CORS configuration in send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -16,12 +16,14 @@ const client = new MongoClient(uri, {
 const app = express();
 
 // CORS configuration
-app.use(cors({
+const corsOptions = {
   origin: 'https://js-form-data-capture.vercel.app', // Your frontend URL
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
-}));
+};
+
+app.use(cors(corsOptions));
 
 // Middleware
 app.use(express.json());
@@ -46,12 +48,11 @@ app.get('/api/test', (req, res) => {
 });
 
 // Define the /send-email endpoint
-app.post('/api/send-email', async (req, res) => {
-  res.header("Access-Control-Allow-Origin", "https://js-form-data-capture.vercel.app");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.options('/api/send-email', cors(corsOptions)); // Enable preflight response
 
+app.post('/api/send-email', cors(corsOptions), async (req, res) => {
   const { name, email, contact, phone } = req.body;
-  
+
   console.log('Received form data:', req.body);
 
   if (!db) {
